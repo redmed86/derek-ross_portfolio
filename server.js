e39cb129eaa45827dee3d51fb78456503bcd6688
Derek Ross <derek.g.ross86@gmail.com>
2021-04-13 16:18:19 -0400

    more base files

--- server.js
+++ server.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const app = express();
+let port = 7222;
 
 app.use(express.static('public'))
 
 app.listen(7222);
-console.log('Application is running on port 7222')
+console.log('Server is running on port ' + port);

69ac6001e1af1e5b02916b419c938a44abdd6d9d
Derek Ross <derek.g.ross86@gmail.com>
2021-04-13 16:35:12 -0400

    tweaking for heroku

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 let port = process.env.PORT || 7222;
 
-app.use(express.static('public'))
+app.use(express.static('public'));
 
-app.listen(7222);
+app.listen(7222, '0.0.0.0');
 console.log('Server is running on port ' + port);
